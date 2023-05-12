255a330f4c321b45e7533a8893a1e46683fdf967
“SanjeevKumarPunia” <drsanjeevpunia@hotmail.com>
2023-05-12 10:35:48 +0530

    commit 2

---
+++ BlockScope.js
@@ -0,0 +1,14 @@
+//for(var i=0;i<=10;i++)
+//{
+ //   console.log(i)
+ //   console.log("Amit how are you")
+//}
+//console.log(i)
+var x=20
+function hello()
+{
+    var x=10
+    console.log(x)
+}
+console.log(x)
+hello()
